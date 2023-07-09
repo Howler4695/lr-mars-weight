9ae73ba65c8b32c70bbf542eff8cb610ee080cb9
hayden howle <howler4695@gmail.com>
2023-07-08 19:22:40 -0500

    done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter weight (kg): ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let userWeight: f32 = input.trim().parse().unwrap();
+    println!("Input: {}", input);
+    let mars_weight = calculate_our_weight_on_mars(userWeight);
+    println!("Weight on Mars: {}", mars_weight);
+}
+
+fn calculate_our_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
 }
